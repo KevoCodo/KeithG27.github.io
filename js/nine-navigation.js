2f7e18565dd806e103813b29b7e2da9eec1837c5
KevoCodo <kevthedev00@gmail.com>
2019-07-26 16:01:11 -0400

    Added 90's Page

---
+++ js/nine-navigation.js
@@ -0,0 +1,20 @@
+document.getElementById('nine-navigation').innerHTML = `
+<!--Navigation-->
+    <nav id='nine-bg' class="navbar navbar-expand-lg navbar-light bg-light p-4">
+        <a class="navbar-brand" href="index.html">KG WatchFacez</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link" href="#">90's Watchfaces</a>
+            <a class="nav-item nav-link" href="#">Pop Watchfaces</a>
+            <a class="nav-item nav-link" href="#">Minimalist Watchfaces</a>
+            <a class="nav-item nav-link" href="#">Gamer Watchfaces</a>
+            <a class="nav-item nav-link" href="#">TV Series Watchfaces</a>
+          </div>
+        </div>
+      </nav>
+    <!--/Navigation-->
+`;
